7bf474e47c6f4a00eea503d7aa13f38afa89733c
Wendel Cutrim <wendelcutrim@hotmail.com>
2023-06-07 19:04:43 -0300

    feat: refatorado o código para realizar a busca dinâmica utilizando o switchMap e pipe async do rxjs e angular

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnDestroy } from "@angular/core";
-import { Subscription } from "rxjs";
+import { Component } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { Observable, switchMap, map, tap } from "rxjs";
 import { LivroVolumeInfo } from "src/app/models/LivroVolumeInfo";
-import { Item, Livro } from "src/app/models/interfaces";
+import { Item } from "src/app/models/interfaces";
 import { LivroService } from "src/app/service/livro.service";
 
 @Component({
@@ -9,32 +10,21 @@ import { LivroService } from "src/app/service/livro.service";
     templateUrl: "./lista-livros.component.html",
     styleUrls: ["./lista-livros.component.css"],
 })
-export class ListaLivrosComponent implements OnDestroy {
-    listaLivros: Livro[] = [];
+export class ListaLivrosComponent {
+    //listaLivros: Livro[] = [];
 
-    campoBusca: string = "";
-    subscription: Subscription;
-    livro: Livro;
+    campoBusca: FormControl = new FormControl();
+
+    livrosEncontrados$: Observable<any> = this.campoBusca.valueChanges.pipe(
+        tap(() => console.log("Fluxo inicial")),
+        switchMap((value) => this.service.buscar(value)),
+        tap(() => console.log("Requisição ao servidor")),
+        map((data) => this.livrosResultadoParaLivros(data)),
+    );
 
     constructor(private service: LivroService) {}
 
-    buscarLivros(): void {
-        this.subscription = this.service.buscar(this.campoBusca).subscribe({
-            next: (data) => {
-                this.listaLivros = this.livrosResultadoParaLivros(data);
-            },
-            error: (err) => {
-                console.error(`[ERROR]: ${JSON.stringify(err)}`);
-            },
-            complete: () => console.log("[INFO]: A requisição foi realizada com sucesso!"),
-        });
-    }
-
     livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
         return items.map((item) => new LivroVolumeInfo(item));
-    }
-
-    ngOnDestroy(): void {
-        this.subscription.unsubscribe();
     }
 }
